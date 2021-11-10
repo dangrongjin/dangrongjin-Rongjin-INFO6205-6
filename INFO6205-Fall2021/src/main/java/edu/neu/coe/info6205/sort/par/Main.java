bfa93c18a0f9657f8aef53e83bd1ab2153474e35
rongjinDang <583365442@qq.com>
2021-11-10 11:45:17 -0500

    assignment5 code has been uploaded.

--- INFO6205-Fall2021/src/main/java/edu/neu/coe/info6205/sort/par/Main.java
+++ INFO6205-Fall2021/src/main/java/edu/neu/coe/info6205/sort/par/Main.java
@@ -16,19 +16,31 @@ import java.util.concurrent.ForkJoinPool;
  */
 public class Main {
 
+    public static int threadCount = 8;
+    public static ForkJoinPool mypool =  new ForkJoinPool(threadCount);
+
     public static void main(String[] args) {
         processArgs(args);
-        System.out.println("Degree of parallelism: " + ForkJoinPool.getCommonPoolParallelism());
+
         Random random = new Random();
-        int[] array = new int[2000000];
+        int[] array = new int[1000000];
         ArrayList<Long> timeList = new ArrayList<>();
-        for (int j = 50; j < 100; j++) {
-            ParSort.cutoff = 10000 * (j + 1);
+        System.out.println("Degree of parallelism: " + mypool.getParallelism()+  "  array length" + array.length + "      except best cutoff :" + array.length/threadCount);
+
+
+        for (int j = 100000; j<1000000; j=j+10000) {
+            int length = 1000000;
+            int count = 0;
+            while(length>j){
+                length= length/2;
+                count++;
+            }
+            ParSort.cutoff = j;
             // for (int i = 0; i < array.length; i++) array[i] = random.nextInt(10000000);
             long time;
             long startTime = System.currentTimeMillis();
             for (int t = 0; t < 10; t++) {
-                for (int i = 0; i < array.length; i++) array[i] = random.nextInt(10000000);
+                for (int i = 0; i < array.length; i++) array[i] = random.nextInt(1000000);
                 ParSort.sort(array, 0, array.length);
             }
             long endTime = System.currentTimeMillis();
@@ -36,7 +48,7 @@ public class Main {
             timeList.add(time);
 
 
-            System.out.println("cutoff：" + (ParSort.cutoff) + "\t\t10times Time:" + time + "ms");
+            System.out.println("cutoff：" + (ParSort.cutoff) + "\t\t10times Time:" + time + "ms" + "\t\trecusion number" + count +"\t\t thread number" + Math.pow(2,count));
 
         }
         try {
